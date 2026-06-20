38f268275ce30b110f68aa789f152805d9dcdefa
Xani <jxesuki@gmail.com>
2026-06-19 19:36:24 -0500

    Switch to non-streaming response and remove get_cwd tool

--- src/chat.py
+++ src/chat.py
@@ -15,60 +15,37 @@ class LlamaChat:
 
         self.available_tools = {
             "read_files": read_files,
-            "list_files": list_files,
-            "get_cwd": get_cwd
+            "list_files": list_files
         }
 
-        self.tool_list = list(self.available_tools.values())
+        self.tool_list = [read_files, list_files]
 
     def get_response(self, messages):
         self.messages = messages
 
-        print("\033[2m[Qwen thinking...] \033[0m",
-              end="", flush=True)
+        print("\033[2m[Qwen thinking...] \033[0m", end="\r")
 
         # Agent Loop
         while True:
-            stream = chat(
+            response = chat(
                 model=self.model,
                 messages=self.messages,
                 tools=self.tool_list,
-                stream=True,
+                think=True,
             )
 
-            in_thinking = False
-            thinking = ""
-            content = ""
-            tool_calls = None
-            for chunk in stream:
-                if chunk.message.thinking:
-                    if not in_thinking:
-                        in_thinking = True
-                    # print(
-                    #     f"\033[2m{chunk.message.thinking}\033[0m", end="", flush=True)
-                    with open("./thinking.txt", "a") as f:
-                        f.write(json.dumps(chunk.model_dump(), indent=4))
-                    thinking += chunk.message.thinking
-                if chunk.message.content:
-                    if in_thinking:
-                        in_thinking = False
-                        print("\nQwen: ", end="", flush=True)
-                    # print(f"{chunk.message.content}",
-                    #       end="", flush=True)
-                    with open("./messageChunks.txt", "a") as f:
-                        f.write(json.dumps(chunk.model_dump(), indent=4))
-                    content += chunk.message.content
-                tool_calls = chunk.message.tool_calls
+            if response.message.thinking:
+                print(
+                    f"\033[2m{response.message.thinking}\033[0m", end="\n")
+            if response.message.tool_calls:
+                self.messages.append(response.message)
 
-            if tool_calls:
-                self.messages.append(stream.message)
-
-                for tool in tool_calls:
+                for tool in response.message.tool_calls:
                     tool_name = tool.function.name
                     arguments = tool.function.arguments
 
                     print(
-                        f"\033[2mQwen: [calling {tool_name} with {arguments}]\033[0m", end="\n")
+                        f"\033[2m[System: AI is running tool {tool_name} with {arguments}] \033[0m", end="\r")
 
                     function_to_call = self.available_tools.get(tool_name)
                     if function_to_call:
@@ -79,12 +56,8 @@ class LlamaChat:
                             "content": str(result),
                             "tool_name": tool_name
                         })
-            else:
-                self.messages = [{
-                    "role": "assistant",
-                    "thinking": thinking,
-                    "content": content
-                }]
 
-            print()
-            break
+            else:
+                print(f"Qwen: {response.message.content}")
+                self.messages.append(response.message)
+                break
